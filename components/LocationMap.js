7216f942f359e0e6057a83eac51f12c2e2a3a59b
nafsuki <natsuki.yamaguchi.62@gmail.com>
2022-02-22 12:55:24 +0100

    revised

--- components/LocationMap.js
+++ components/LocationMap.js
@@ -42,16 +42,16 @@ export default function LocationMap() {
 				className='mr-2 md:mr-4 lg:mr-10 absolute top-2 right-0 hover:bg-almostBlack w-[60px] h-[60px] flex justify-center items-center rounded-full bg-gold cursor-pointer transition fill-white stroke-white'
 			>
 				<svg
-					class='w-6 h-6'
+					className='w-6 h-6'
 					// fill='none'
 					// stroke='currentColor'
 					viewBox='0 0 24 24'
 					xmlns='http://www.w3.org/2000/svg'
 				>
 					<path
-						stroke-linecap='round'
-						stroke-linejoin='round'
-						stroke-width='2'
+						strokeLinecap='round'
+						strokeLinejoin='round'
+						strokeWidth='2'
 						d='M19 14l-7 7m0 0l-7-7m7 7V3'
 					></path>
 				</svg>
