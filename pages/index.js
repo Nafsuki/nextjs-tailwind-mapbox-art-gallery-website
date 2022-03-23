ed7b21b0ff2143f48b9728458d7ecda66b06ac80
Marcin - Windows 11 <nodexchange@outlook.com>
2022-03-23 21:30:53 +0000

    new logo

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,14 @@ import HeroSection from "../components/HeroSection";
 import MainSection from "../components/MainSection";
 import Layout from "../components/Layout";
 import Signup from "../components/Signup";
-import ScrollDownIcon from '../components/ScrollDownIcon';
+import ScrollDownIcon from "../components/ScrollDownIcon";
 
 export default function Home() {
   return (
     <Layout>
       {/* <HeroVideo /> */}
       <HeroSection />
-      <Signup />
+      {/* <Signup /> */}
       <MainSection />
     </Layout>
   );
