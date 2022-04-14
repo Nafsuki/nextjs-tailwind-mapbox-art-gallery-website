954a950a08994bfcabad6103c33a0a779ce0676c
Wotjala <marcin.wotjala@bottomline.com>
2022-04-14 17:45:44 +0100

    instructors section

--- components/Instructors.js
+++ components/Instructors.js
@@ -1,22 +1,41 @@
-import Link from "next/link";
-import text from "../config/text.json";
+import Link from 'next/link';
+import Image from 'next/image';
+import text from '../config/text.json';
 
 export const Instructors = () => {
-  const { footer } = text;
+  const { instructors } = text;
+  console.log('instructors', instructors);
   return (
-    <main className="flex flex-row md:flex-row bg-white text-almostBlack px-8 py-10 md:py-20 lg:py-30 lg:px-40 justify-between md:items-start">
-      <h3
-        className="text-bodyM font-black uppercase font-bigShoulder cursor-pointer"
-        style={{ lineHeight: "1.5rem" }}
-      >
-        our instructors
-      </h3>
-      <p className="my-12 md:m-0 font-light font-outfit text-bodyXS md:text-bodyS md:w-2/5">
-        Alyssa is a certified personal trainer and instructor of all levels.
-        <br />
-        <br />
-        Martin is a certified personal trainer and instructor of all levels.
-      </p>
+    <main className=" bg-shine text-white px-8 py-10 md:py-20 lg:py-30 lg:px-40 justify-between md:items-start">
+        <div className="block lg:flex">
+          <div className="flex-none w-[445px] h-[889px]">
+            <Image
+              src="/desktop/instructors.jpg"
+              width={500}
+              height={889}
+              objectFit="cover"
+              layout="responsive"
+              alt="main grid image desktop 3"
+              priority="true"
+            />
+          </div>
+          <div className="grow px-4 pt-0 lg:pt-0">
+            <h2 className="font-black uppercase font-bigShoulder text-headingS lg:text-headingS md:mb-4">
+              Meet your teachers
+            </h2>
+            <p className="my-4 l:my-12 md:m-0 font-light font-outfit text-bodyXS md:text-bodyS md:w-100">
+            {instructors.first}
+            </p>
+            <br />
+            <p className="my-4 l:my-12 md:m-0 font-light font-outfit text-bodyXS md:text-bodyS md:w-100">
+              {instructors.second}
+            </p>
+            <br />
+            <p className="my-4 l:my-12 md:m-0 font-light font-outfit text-bodyXS md:text-bodyS md:w-100">
+              {instructors.third}
+            </p>
+          </div>
+        </div>
     </main>
   );
-}
+};
--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,8 @@ export default function Home() {
       <CourseInfo />
       {/* <Signup /> */}
       <MainSection />
+      <Instructors />
       {/* <ImageGallery /> */}
-      {/* <Instructors /> */}
     </Layout>
   );
 }
